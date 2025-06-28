526b86817e51e01041034cd585c593e332d1293a
Viktor <viktoryanenko11@gmail.com>
2025-06-28 15:45:18 +0300

    upd client NotePreview

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+import NotePreview from '../../../../components/NotePreview/NotePreview';
+import Modal from '../../../../components/Modal/Modal';
+
 export default function NotePreviewClient() {
-  return <></>;
+  const { id } = useParams<{ id: string }>();
+
+  return (
+    <Modal>
+      <NotePreview id={Number(id)} />
+    </Modal>
+  );
 }
